2d1966e3d22a3f78abea09a7791d98b32038fbe3
shawon hridoy <mohammadshawon781@gmail.com>
2022-03-24 07:57:08 -0700

    I have added 6 variable in this file.It is assignment 2 of my php course.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+//  Case Study:
+
+// I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
+
+// For creating a product upload form what specifications will be uploaded and what will be their data types?
+
+
+$monitor_brand="hp"; //data type string
+$monitor_condition= "used"; //data type string
+$monitor_quantity= 3; //data type decimal
+$monitor_display_size=15.6; //data type float/double
+$is_vga_included = true;//data type boolean
+$price_of_price=8000; // data type decimal 
+
+
+?>
+
